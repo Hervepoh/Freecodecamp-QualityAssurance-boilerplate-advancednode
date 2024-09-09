bc36ef5eff766686e79ad9169a11597cd1608aa8
Ngando Epoh Herve <epohherve63@gmail.com>
2024-09-09 20:55:50 +0100

    Add Passport Github strategy

---
+++ auth.js
@@ -0,0 +1,48 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const GitHubStrategy = require("passport-github").Strategy;
+const bcrypt = require("bcrypt");
+const { ObjectID } = require("mongodb");
+require("dotenv").config();
+
+module.exports = function (app, myDataBase) {
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      if (err) return console.error(err);
+      done(null, doc);
+    });
+  });
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
+
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "https://3000-freecodecam-boilerplate-2hl2tc3qkg1.ws-eu116.gitpod.io/auth/github/callback"/*INSERT CALLBACK URL ENTERED INTO GITHUB HERE*/
+  },
+    function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      //Database logic here with callback containing your user object
+    }
+  ));
+
+  
+};
